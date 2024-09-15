cd6b51e018a727b2323031fd83e762b445354bba
ayuaa13 <ayuzr13@gmail.com>
2024-09-15 10:53:43 +0800

    Add files via upload

---
+++ 2409106022_Ayu Azzhahrah Alwi_POSTTES 2.py
@@ -0,0 +1,23 @@
+#Membuat biodata sederhana
+
+print("Biodata saya")
+
+Nama_Lengkap = input("Nama Lengkap: ")
+Nama_Panggilan = input("Nama Panggilan: ")
+Fakultas = input("Fakultas: ")
+Prodi = input("Prodi: ")  
+NIM = int(input("NIM: "))
+Umur = float(input("Umur: "))
+Tinggi_Badan = float(input("Tinggi Badan dalam satuan M: "))
+Jarak_Kampus = float(input("Jarak dari rumah atau kos ke Kampus dalam satuan KM: "))
+#list
+Cita_cita = ["Software engineer","Software developer", "System analyst", "IT consultant","Cyber security specialist"]
+
+print(f"Nama Saya",Nama_Lengkap,"saya biasa dipanggil",Nama_Panggilan,"saya dari fakultas",Fakultas,"prodi",Prodi,"dengan NIM",NIM)
+print(f"Umur Saya Tahun ini",Umur,"Tahun","Tinggi badan saya",Tinggi_Badan,"M","dan Jarak dari Rumah atau kos Saya ke Kampus",Jarak_Kampus,"KM")
+print(f"cita-cita saya adalah",Cita_cita[1])
+
+Nim_Terakhir = NIM % 1000
+Nim_Modulus = Nim_Terakhir % 6
+
+print(f"3 angka terakhir pada NIM saya setelah dimoduluskan dengan 6 adalah",Nim_Modulus)
