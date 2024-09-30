7a02e2a5a174f9ac5e753b0d3cc4387b2d1e6996
ayuaa13 <ayuzr13@gmail.com>
2024-09-30 20:59:59 +0800

    Delete Praktikum-APD/Kelas A1'24 Informatika directory

--- Praktikum-APD/Kelas A1'24 Informatika/Pertemuan 3/pertemuan 3 (1).py
+++
@@ -1,8 +0,0 @@
-cuaca = str(input("Masukkan cuaca hari ini: "))
-
-if cuaca == "cerah":
-    print("belajar di sekolah")
-elif cuaca == "Hujan":
-    print("belajar dirumah")
-else:
-    print("invalid")
