af2870eff8fa703ebbebc410e7043570e0cd6a15
ayuaa13 <ayuzr13@gmail.com>
2024-10-01 14:23:22 +0800

    Delete Praktikum-APD/Kelas A1'24 Informatika directory

--- Praktikum-APD/Kelas A1'24 Informatika/Pertemuan-3/Operator ternary.py
+++
@@ -1,4 +0,0 @@
-'''Operator ternary seperti if tapi cuman 1 baris'''
-angka = int(input("Masukkan angka: "))
-result = "Genap" if angka % 2 == 0 else "Ganjil"
-print(angka, "adalah angka",result)
--- Praktikum-APD/Kelas A1'24 Informatika/Pertemuan-3/pertemuan 3.py
+++
@@ -1,8 +0,0 @@
-angka = int(input("Masukkan angka: "))
-
-if angka > 30:
-    print("sapi")
-    if angka > 50:
-        print("singa")
-else:
-    print("invalid")
